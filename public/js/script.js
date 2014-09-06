7f3ade30af303c2c8219adf0c4c689c179299463
jianhe25 <jianhe25@google.com>
2014-09-06 12:07:43 +0800

    name jquery object with $, load caption ok

--- public/js/script.js
+++ public/js/script.js
@@ -4,26 +4,26 @@ $(document).ready(function(){
 var video = document.getElementById('video');
 
 // Buttons
-var playButton = $('.js_play_pause');
-var volumeIndicator = $('.js_volume_indicator');
+var $playButton = $('.js_play_pause');
+var $volumeIndicator = $('.js_volume_indicator');
 
 // Sliders
-var seekBar = $('.seek-bar');
-var cursor = $('.js_cursor');
-var progressBar = $('.js_progress_bar');
-var volumeBar = $('.js_volume_bar');
-var volumeBarInner = $('.js_volume_bar_inner');
+var $seekBar = $('.seek-bar');
+var $cursor = $('.js_cursor');
+var $progressBar = $('.js_progress_bar');
+var $volumeBar = $('.js_volume_bar');
+var $volumeBarInner = $('.js_volume_bar_inner');
 
-var fullScreenButton = $('.js_full_screen');
-var addAdbreakButton = $('.js_add_adbreak');
-var timeIndicatorSpent = $('.js_time_spent');
-var timeIndicatorTotal = $('.js_time_total');
-var adbreakHandlerContainer = $('.adbreak-handler-container');
-var adbreakButton = $('.js_adbreak_button');
-var adbreakRemove = $('.js_adbreak_remove');
-var captionHandler = $('.js_caption_handler');
-var captionTimestamp = $('.js_caption_timestamp');
-var caption = $('.js_caption');
+var $fullScreenButton = $('.js_full_screen');
+var $addAdbreakButton = $('.js_add_adbreak');
+var $timeIndicatorSpent = $('.js_time_spent');
+var $timeIndicatorTotal = $('.js_time_total');
+var $adbreakHandlerContainer = $('.adbreak-handler-container');
+var $adbreakButton = $('.js_adbreak_button');
+var $adbreakRemove = $('.js_adbreak_remove');
+var $captionHandler = $('.js_caption_handler');
+var $captionTimestamp = $('.js_caption_timestamp');
+var $caption = $('.js_caption');
 
 var videoPlayer = new HP.VideoPlayer.BaseVideoPlayer(new HP.VideoPlayer.MediaElement(document.getElementById('video')));
 
@@ -40,16 +40,17 @@ var videoController = (function() {
     video.addEventListener('timeupdate', onVideoTimeUpdate);
     video.addEventListener('ended', my.pause);
     this.play();
-    playButton.click(onPlayButtonClick);
+    $playButton.click(onPlayButtonClick);
   };
   
   function onVideoTimeUpdate() {
     // Calculate the slider value
-    var value = (seekBar.width() / video.duration) * video.currentTime;
+    var value = ($seekBar.width() / video.duration) * video.currentTime;
     // Update cursor value
-    progressBar.update(value);
-    timeIndicatorSpent.text(utils.convertValueToTime(video.currentTime));
-    timeIndicatorTotal.text(utils.convertValueToTime(video.duration));
+    $progressBar.update(value);
+    $timeIndicatorSpent.text(utils.convertValueToTime(video.currentTime));
+    $timeIndicatorTotal.text(utils.convertValueToTime(video.duration));
+    captionController.updateCaptionByTime(video.currentTime);
   };
 
   // Event listener for the play/pause button
@@ -63,13 +64,13 @@ var videoController = (function() {
 
   my.play = function() {
     video.play();
-    playButton.removeClass('fa-play');
-    playButton.addClass('fa-pause');
+    $playButton.removeClass('fa-play');
+    $playButton.addClass('fa-pause');
   };
   my.pause = function() {
     video.pause();
-    playButton.addClass('fa-play');
-    playButton.removeClass('fa-pause');
+    $playButton.addClass('fa-play');
+    $playButton.removeClass('fa-pause');
   };
   return my;
 })();
@@ -84,71 +85,71 @@ var onFullScreenButtonClick = function() {
     video.webkitRequestFullscreen(); // Chrome and Safari
   }
 };
-fullScreenButton.click(onFullScreenButtonClick);
+$fullScreenButton.click(onFullScreenButtonClick);
 
 var seekBarController = {
   init: function() {
-    seekBar.click(seekBarController.onSeekBarClick);
-    seekBar.mousemove(seekBarController.onSeekBarMouseMove);
-    cursor.mousemove(seekBarController.onCursorMouseMove)
-    cursor.move = function (value) {
-      cursor.css('left', value);
+    $seekBar.click(this.onSeekBarClick);
+    $seekBar.mousemove(this.onSeekBarMouseMove);
+    $cursor.mousemove(this.onCursorMouseMove);
+    $cursor.move = function (value) {
+      $(this).css('left', value);
     };
-    progressBar.update = function(value) {
-      progressBar.css('width', value);
+    $progressBar.update = function(value) {
+      $(this).css('width', value);
     };
-    seekBar.on('dblclick', '.js_adbreak', seekBarController.onSeekBarDoubleClick);
+    $seekBar.on('dblclick', '.js_adbreak', this.onSeekBarDoubleClick);
   },
   onSeekBarClick: function(e) {
-    var offsetX = e.pageX - seekBar.offset().left;
-    cursor.move(offsetX);
-    var time = video.duration * (offsetX / seekBar.width());
+    var offsetX = e.pageX - $(this).offset().left;
+    $cursor.move(offsetX);
+    var time = video.duration * (offsetX / $(this).width());
     video.currentTime = time;
     console.log('time = ' + time + ' video.currentTime = ' + video.currentTime);
   },
   onSeekBarMouseMove: function(e) {
-    var offsetX = e.pageX - seekBar.offset().left;
-    cursor.move(offsetX);
+    var offsetX = e.pageX - $(this).offset().left;
+    $cursor.move(offsetX);
   },
   onCursorMouseMove: function(e) {
     e.stopPropagation();
   },
   onSeekBarDoubleClick: function(e) {
     var offset = parseInt( $(e.currentTarget).css('left') ) + 1;
-    var time = video.duration * (offset / seekBar.width());
+    var time = video.duration * (offset / $(this).width());
     video.currentTime = time;
   }
 };
 
 var volumeController = {
   init: function() {
-    volumeIndicator.mouseenter(this.onVolumeIndicatorMouseEnter);
-    volumeIndicator.mouseleave(this.onVolumeIndicatorMouseLeave);
-    volumeBar.mousedown(this.onVolumeBarMouseDown)
+    $volumeIndicator.mouseenter(this.onVolumeIndicatorMouseEnter);
+    $volumeIndicator.mouseleave(this.onVolumeIndicatorMouseLeave);
+    $volumeBar.mousedown(this.onVolumeBarMouseDown)
          .mouseup(this.onVolumeBarMouseUp)
          .mouseleave(this.onVolumeBarMouseLeave);
-    video.volume = (volumeBar.height() - volumeBarInner.height()) / volumeBar.height();
+    video.volume = ($volumeBar.height() - $volumeBarInner.height()) / $volumeBar.height();
   },
   onVolumeIndicatorMouseEnter: function() {
-    volumeBar.removeClass('hidden');
+    $volumeBar.removeClass('hidden');
   },
   onVolumeIndicatorMouseLeave: function() {
-    volumeBar.addClass('hidden');
+    $volumeBar.addClass('hidden');
   },
   onVolumeBarMouseDown: function(e) {
     volumeController.updateVolumeInnerBar(e);
-    volumeBar.on('mousemove', volumeController.updateVolumeInnerBar);
+    $volumeBar.on('mousemove', volumeController.updateVolumeInnerBar);
   },
   onVolumeBarMouseUp: function(e) {
-    volumeBar.unbind('mousemove');
+    $volumeBar.unbind('mousemove');
   },
   onVolumeBarMouseLeave: function(e) {
-    volumeBar.unbind('mousemove');
+    $volumeBar.unbind('mousemove');
   },
   updateVolumeInnerBar: function(e) {
-    var offsetY = e.pageY - volumeBar.offset().top;
-    volumeBarInner.css('height', offsetY);
-    var volume = (volumeBar.height() - offsetY) / volumeBar.height();
+    var offsetY = e.pageY - $volumeBar.offset().top;
+    $volumeBarInner.css('height', offsetY);
+    var volume = ($volumeBar.height() - offsetY) / $volumeBar.height();
     video.volume = volume;
     /*
       if (volume > 0.5) {
@@ -176,7 +177,7 @@ var keyController = {
       }
       // space
       if (event.keyCode == 32) {
-        playButton.click();
+        $playButton.click();
       }
     });
   }
@@ -185,28 +186,28 @@ var keyController = {
 var adBreakController = {
   numAdbreaks: 0,
   init: function() {
-    addAdbreakButton.click(this.onAddAdBreakButtonClick);
-    adbreakButton.click(this.onAdBreakButtonClick);
-    adbreakRemove.click(this.onAdbreakRemoveClick);
+    $addAdbreakButton.click(this.onAddAdBreakButtonClick);
+    $adbreakButton.click(this.onAdBreakButtonClick);
+    $adbreakRemove.click(this.onAdbreakRemoveClick);
   },
   onAddAdBreakButtonClick: function(e) {
     var position = cursor.css('left');
     position = parseInt(position) - 1;
     console.log('position: ' + position);
     this.numAdbreaks++;
-    var new_adbreak = $('.adbreak').first().clone(/*withDataAndEvents=*/true);
-    new_adbreak.attr('id', 'adbreak_' + this.numAdbreaks);
-    new_adbreak.css('left', position);
-    new_adbreak.removeClass('adbreak-template');
-    new_adbreak.appendTo(seekBar);
+    var $new_adbreak = $('.adbreak').first().clone(/*withDataAndEvents=*/true);
+    $new_adbreak.attr('id', 'adbreak_' + this.numAdbreaks);
+    $new_adbreak.css('left', position);
+    $new_adbreak.removeClass('adbreak-template');
+    $new_adbreak.appendTo(seekBar);
 
-    var new_adbreak_handler = $('.js_adbreak_handler').first().clone(/*withDataAndEvents=*/true);
+    var $new_adbreak_handler = $('.js_adbreak_handler').first().clone(/*withDataAndEvents=*/true);
     var adbreak_time = (position+1) / seekBar.width() * video.duration;
-    new_adbreak_handler.children('.js_adbreak_button').val( utils.convertValueToTimeMs(adbreak_time) );
-    new_adbreak_handler.removeClass('adbreak-handler-template');
+    $new_adbreak_handler.children('.js_adbreak_button').val( utils.convertValueToTimeMs(adbreak_time) );
+    $new_adbreak_handler.removeClass('adbreak-handler-template');
 
-    new_adbreak_handler.data('index', this.numAdbreaks);
-    new_adbreak_handler.appendTo(adbreakHandlerContainer);
+    $new_adbreak_handler.data('index', this.numAdbreaks);
+    $new_adbreak_handler.appendTo(adbreakHandlerContainer);
   },
   onAdBreakButtonClick: function(e) {
     console.log('adbreakHandler = ' + utils.convertTimeToValueMs($(this).val()) );
@@ -226,35 +227,53 @@ function getMethods(obj) {
   for (var id in obj) {
     try {
       if (typeof(obj[id]) == "function") {
-        result.push(id + ": " + obj[id].toString());
+        console.log(id + ": " + obj[id].toString());
       }
     } catch (err) {
-      result.push(id + ": inaccessible");
+      console.log(id + ": inaccessible");
     }
   }
   return result;
 }
 
-var captionController = {
-  init: function() {
-    this.prepareTestCaption();
-    this.loadCaption();
-    captionTimestamp.click(function(e) {
-      caption.text($(this).next().val());
+var captionController = (function(){
+  var my = {};
+  captions = [];
+  function Caption(timestamp, line) {
+    this.timestamp = timestamp;
+    this.line = line;
+  };
+
+  Caption.prototype.compare = function(other) {
+    if (this.timestamp < other.timestamp)
+      return -1;
+    else if (this.timestamp == other.timestamp)
+      return 0;
+    else
+      return 1;
+  };
+
+  my.init = function() {
+    prepareTestCaption();
+    loadCaption();
+    $captionTimestamp.click(function(e) {
+      $caption.text($(this).next().val());
       video.currentTime = utils.convertTimeToValueMs( $(this).text() );
     });
-  },
-  prepareTestCaption: function() {
+  };
+
+  function prepareTestCaption() {
     for (var i = 2; i <= 10; ++i) {
-      var new_caption_handler = captionHandler.clone(/*withDataAndEvents=*/true, /*deepWithDataAndEvents=*/true);
-      var old_time = new_caption_handler.children('.js_caption_timestamp').text();
+      var $new_caption_handler = $captionHandler.clone(/*withDataAndEvents=*/true, /*deepWithDataAndEvents=*/true);
+      var old_time = $new_caption_handler.children('.js_caption_timestamp').text();
       var new_time = utils.convertValueToTimeMs(utils.convertTimeToValueMs(old_time) + i);
-      new_caption_handler.children('input').val(i + '-th line of caption');
-      new_caption_handler.children('.js_caption_timestamp').text(new_time);
-      new_caption_handler.appendTo(captionHandler.parent());
+      $new_caption_handler.children('input').val(i + '-th line of caption');
+      $new_caption_handler.children('.js_caption_timestamp').text(new_time);
+      $new_caption_handler.appendTo($captionHandler.parent());
     }
-  },
-  loadCaption: function() {
+  };
+
+  function loadCaption() {
     console.log('starting load caption');
     $.ajax({
       url: 'static/captions/test.smi',
@@ -262,9 +281,15 @@ var captionController = {
       dataType: 'text'
     })
     .done(function(data) {
-      // var jq = $(data);
-      // var node = jq.children('sync');
-      // console.log(node[0]);
+      var jq = $(data);
+      var nodes = jq.children('sync');
+
+      for (var i = 0; i < nodes.length; ++i) {
+        var timestamp = parseInt($(nodes[i]).attr("start")) / 10000.0;
+        var line = $(nodes[i]).html();
+        captions.push( new Caption(timestamp, line) );
+      }
+
       // var domNodes = $.parseHTML(data);
       // // domNodes.length is 1
       // var html = domNodes[0];
@@ -278,8 +303,34 @@ var captionController = {
       console.log("loading caption failed");
     });
     console.log('end load caption ajax call');
-  }
-};
+  };
+
+  function binarySearch(captions, nowTime) {
+    console.log(captions.length);
+    var l = 0, r = captions.length - 1;
+    while (l <= r) {
+      var mid = Math.round( (l+r) / 2 );
+      console.log(mid, captions[mid]);
+      if (captions[mid].compare(nowTime) == -1) {
+        l = mid + 1;
+      } else {
+        r = mid - 1;
+      }
+    }
+    if (l == 0)
+      return new Caption(0, "");
+    else
+      return captions[l-1];
+  };
+  
+  my.updateCaptionByTime = function(time) {
+    if (captions.length > 0) {
+      var caption = binarySearch(captions, new Caption(time));
+      $caption.html(caption.line);
+    }
+  };
+  return my;
+})();
 
 var utils = {
   PaddingZero: function (num, size) {
